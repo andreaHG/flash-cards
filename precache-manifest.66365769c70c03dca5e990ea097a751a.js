151d3f71db8d60c3a617606dadf9c3110bd65201
AndreaHG <andrea.hrnndzg@gmail.com>
2019-03-04 19:26:43 -0500

    Updates

---
+++ precache-manifest.66365769c70c03dca5e990ea097a751a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/flash-card-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9aba95e0201946540a3f",
+    "url": "/flash-card-game/static/js/runtime~main.9aba95e0.js"
+  },
+  {
+    "revision": "eb6e98c461cfcd698b25",
+    "url": "/flash-card-game/static/js/main.eb6e98c4.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/flash-card-game/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "eb6e98c461cfcd698b25",
+    "url": "/flash-card-game/static/css/main.c041f2dc.chunk.css"
+  },
+  {
+    "revision": "94ef250559f0c24433860cb5116e7f2d",
+    "url": "/flash-card-game/index.html"
+  }
+];
